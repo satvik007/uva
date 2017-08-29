4012b1203aacbcae4270a33c9a91154b6a1b5328
satvik007 <111601021@smail.iitpkd.ac.in>
2017-08-29 16:25:41 +0530

    new

--- Data_structures/Bit Manipulation/multitasking.cpp
+++ Data_structures/Bit Manipulation/multitasking.cpp
@@ -12,9 +12,41 @@ int main(){
 	cin.tie(NULL);
 	//ifstream cin("in.txt");
 	//ofstream cout("out.txt");
+	vector <bool> status(1000001);
 	int n, m;
 	while(cin >> n >> m, n||m){
-		
+		vector <Data> a(n);
+		vector <Data> b(m);
+		fill(status.begin(), status.end(), false);
+		for(int i=0; i<n; i++) cin >> a[i].start >> a[i].end;
+		for(int i=0; i<m; i++) cin >> b[i].start >> b[i].end >> b[i].rep;
+		int flag = 0;
+		for(int i=0; i<n; i++){
+			for(int j=a[i].start; j<a[i].end; j++){
+				if(status[j] == true){
+					flag = 1;
+					break;
+				}
+				else status[j] = true;
+			}
+			if(flag == 1) break;
+		}
+		for(int i = 0; i<m; i++){
+			int duration = b[i].end - b[i].start;
+			for(int j=b[i].start; j<=1000000; j += b[i].rep){
+				for(int k=j; k<j+duration && k < 1000001; k++){
+					if(status[k] == true){
+						flag = 1;
+						break;
+					}
+					else status[k] = true;
+				}
+				if(flag == 1) break;
+			}
+			if(flag == 1) break;
+		}
+		if(flag == 0) cout << "NO CONFLICT\n";
+		else cout << "CONFLICT\n";
 	}
 	
 	return 0;
