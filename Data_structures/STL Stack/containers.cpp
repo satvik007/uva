bff66618faac18ab8e960f690e39c4174e975b36
satvik007 <111601021@smail.iitpkd.ac.in>
2017-09-01 11:47:58 +0530

    new

--- Data_structures/STL Stack/containers.cpp
+++ Data_structures/STL Stack/containers.cpp
@@ -9,14 +9,23 @@ int main(){
 	ofstream cout("out.txt");
 	string a;
 	int cas = 1;
+	
 	while(cin >> a){
 		if(a == "end") break;
 		cout << "Case " << cas << ": ";
-		vector <int> status(26);
-		fill(status.begin(), status.end(), -1);
-		for(int i=a.size()-1; i>=0; i--){
-			if(status[a[i]-'A'] == -1) status[a[i]-'A'] = i;
+		vector <stack<char>> b(26);
+		int counter = 1;
+		for(int i=0; i<a.size(); i++){
+			for(int j=0; j<26; j++){
+				counter = max(counter, j+1);
+				if(b[j].empty() || b[j].top() >= a[i]){
+					b[j].push(a[i]);
+					break;
+				}
+			}
 		}
+		cout << counter << "\n";
+		
 		cas++;
 	}
 }
