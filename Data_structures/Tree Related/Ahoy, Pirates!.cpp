db28838551ad188595fab8845925cf7e124825aa
Satvik Choudhary <111601021@smail.iitpkd.ac.in>
2017-09-07 09:02:16 +0530

    Update Ahoy, Pirates!.cpp

--- Data_structures/Tree Related/Ahoy, Pirates!.cpp
+++ Data_structures/Tree Related/Ahoy, Pirates!.cpp
@@ -1,29 +1,48 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 typedef vector <int> vi;
 
-class SegmentTree{
-private: vi A, st;
+vector <int> a;
+
+class node{
 public:
-    int n;
-    SegmentTree(const vi &_A){
-        A = _A; n = A.size();
-        st.assign(4*n, 0);
+	int value, len;
+	char op;
+};
+
+class SegmentTree{
+private: vi A;
+	vector <node> st;
+	int n;
+public:
+    int left(int p) {return (p << 1);}
+    int right(int p) {return ((p << 1)|1);}
+	explicit SegmentTree(){
+		n = a.size();
+		st.resize(4*n);	
         build(1, 0, n-1);
-    }
-    int left(int p) {return (p<<1);}
-    int right(int p) {return ((p<<1)+1);}
+	}
     int build(int p, int L, int R){
-        if(L==R) return st[p] = A[L];
-        return st[p] = max(build(left(p), L, (L+R)/2), build(right(p), (L+R)/2+1, R));
+        st[p].len = R-L+1;
+        st[p].op = -1;
+        if(L == R) return st[p].value = a[L];
+        else return st[p].value = build(left(p), L, (L+R)/2) + build(right(p), (L+R)/2, R); 
     }
-    int rmq(int p, int L, int R, int i, int j) {
-        if (i > R || j < L) return -1;
-        if (L >= i && R <= j) return st[p];
-        return max(rmq(left(p), L, (L+R)/2, i, j), rmq(right(p), (L+R)/2+1, R, i, j));
+    int rmq(int p, int L, int R, int i, int j){
+        apply_update(p, i, j);
+        if(j<L || i>R) return 0;
+        if(i<=L && j>=R) return st[p].value;
+        return rmq(left(p), L, (L+R)/2, i, j) + rmq(right(p), (R+L)/2+1, R, i, j);
     }
-    int rmq(int i, int j){return rmq(1, 0, n-1, i, j);}
+    int rmq(int i, int j){
+        return rmq(1, 0, n-1, i, j);
+    }
+    void update(int p, int L, int R, int u, int v, char wish){
+        if(j<L || i>R) return;
+        if(i<=L && j>=R) return;
+        update(left(p)
+    }
+    void update(int i, int j, char wish){return update(1, 0, n-1, i, j, wish);}
 };
 
 int main(){
@@ -50,19 +69,9 @@ int main(){
         int k = 1;
         for(int i=0; i<q; i++){
             cin >> wish >> u >> v;
-            if(wish == 'F'){
-                
-            }
-            else if(wish == 'E'){
-
-            }
-            else if(wish == 'I'){
-
-            }
-            else if(wish == 'S'){
-
-                cout << "Q" << (k++) << " " << "\n";
-            }
+            u--; v--;
+            if(wish == 'S') cout << "Q" << (k++) << " " << seg.rmq(u, v) << "\n";
+            else update(u, v, wish);   
         }
     }
 }
