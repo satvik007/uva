af5013f3ac1a1fc4ca7be6eae2ca9f7ff282bec9
satvik007 <111601021@smail.iitpkd.ac.in>
2017-09-12 01:50:31 +0530

    new

--- Problem Solving Paradigms/Complete Search/Iterative 3+ harder/Citizen attention offices.cpp
+++ Problem Solving Paradigms/Complete Search/Iterative 3+ harder/Citizen attention offices.cpp
@@ -1,52 +1,47 @@
 #include <bits/stdc++.h>
-#define epsilon 1e-9
 using namespace std;
 typedef long long ll;
 typedef vector <int> vi;
-typedef vector <ll> vll;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
-    int t, u, v, w, size = 25, counter, index, n;
-    vector <vi> a(5, vi(5));
-    vi c;
+    int t, u, v, w, size = 25, counter, n, min1;
     cin >> t;
+    vi a(25);
+    vi b(5);
+    vi ans(5);
     while(t--){
         cin >> n;
-        c.clear();
-        for(int i=0; i<5; i++){
-        	a[i].assign(5, 0);
-		}
+        a.assign(25, 0);
         for(int i=0; i<n; i++){
             cin >> u >> v >> w;
-            a[u][v] = w;
+            a[5*u + v] = w;
         }
-        for(int i=0; i<size-4; i++){
-        	for(int j=0; j<size-3; j++){
-        		for(int r=0; r<size-2; r++){
-        			for(int m=0; m<size-1; m++){
-        				for(int p=0; p<size; p++){
-        					counter = 0;
-        					for(int x = 0; x<5; x++){
-        						for(int y=0; y<5; y++){
-        							counter += a[x][y]*((abs(x-i/5)+abs(y-i%5)) + (abs(x-j/5)+abs(y-j%5)) + (abs(x-r/5)+abs(y-r%5))+(abs(x-m/5)+abs(y-m%5))+(abs(x-p/5)+abs(y-p%5)));
-								}
-							}
-							c.push_back(counter);
-						}
-					}
-				}
-			}
-		}
-		sort(c.begin(), c.end());
-		auto it = unique (c.begin(), c.end());
-		c.resize(it - c.begin());
-        cout << c[0];
-        for(int i=1; i<5; i++) cout << " " << c[i];
-		cout << "\n";
+
+        min1 = INT_MAX;
+        for(int i=0; i<size-4; i++){ for(int j=i+1; j<size-3; j++){ for(int r=j+1; r<size-2; r++){ for(int m=r+1; m<size-1; m++){ for(int p=m+1; p<size; p++){
+                            counter = 0;
+                            for(int s = 0; s<25; s++){
+                                if(a[s]){
+                                    b[0] = abs(i/5-s/5) + abs(i%5 - s%5);
+                                    b[1] = abs(j/5-s/5) + abs(j%5 - s%5);
+                                    b[2] = abs(r/5-s/5) + abs(r%5 - s%5);
+                                    b[3] = abs(m/5-s/5) + abs(m%5 - s%5);
+                                    b[4] = abs(p/5-s/5) + abs(p%5 - s%5);
+                                    counter += a[s] * (*min_element(b.begin(), b.end()));
+                                }
+                            }
+                            if(counter < min1){
+                                //cout << counter << endl;
+                                min1 = counter;
+                                ans[0] = i; ans[1] = j; ans[2] = r; ans[3] = m; ans[4] = p;
+                            }
+                        }}}}}
+        cout << ans[0];
+        for(int i=1; i<5; i++) cout << " " << ans[i]; cout << "\n";
     }
     return 0;
 }
