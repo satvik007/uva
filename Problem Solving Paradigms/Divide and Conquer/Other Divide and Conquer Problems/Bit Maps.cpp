8ea5d175b057e6df8ad05d7f91801f7cbb0fd42d
satvik007 <111601021@smail.iitpkd.ac.in>
2017-09-18 12:47:30 +0530

    new

---
+++ Problem Solving Paradigms/Divide and Conquer/Other Divide and Conquer Problems/Bit Maps.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int;
+typedef vector <int> vi;
+
+char type;
+int n, m;
+string b;
+
+int btod(){
+	
+}
+
+int dtob(){
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	while(cin >> type, type!='#'){
+		cin >> n >> m >> b;
+			
+	}
+	return 0;
+}
